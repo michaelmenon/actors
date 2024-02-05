300d01e0f025666e0f940a1c072fd50d2b17a842
michaelmenon <midhun@Midhuns-MacBook-Pro.local>
2024-02-05 17:45:36 -0500

    updated load test function

--- actors_test.go
+++ actors_test.go
@@ -72,30 +72,32 @@ func TestActorReceiveMessage(t *testing.T) {
 }
 
 func TestSendMessage(b *testing.T) {
+	actors := make([]*Actor, 0, 100)
+	//Add a million actors
+	for i := 0; i < 1000000; i++ {
+		id := fmt.Sprintf("%d", i)
+		act1, _ := NewActor(id)
+		actors = append(actors, act1)
 
-	id1 := "testactor1"
-	act1, _ := NewActor(id1)
-	// Check if the result is not nil
-	if act1 == nil {
-		b.FailNow()
 	}
-	id2 := "testactor2"
-	act2, _ := NewActor(id2)
-	// Check if the result is not nil
-	if act2 == nil {
-		b.FailNow()
-	}
+	b.Log("created all actors")
+	b.Log(len(actors))
 	close := "CLOSE"
+	id := fmt.Sprintf("%d", 5)
+	t1 := time.Now()
 	go func() {
 
-		for i := 0; i < 1000000; i++ {
+		for i, act := range actors {
 
-			err := act2.SendMessage(id1, []byte(fmt.Sprintf("%d", i)))
+			if i == 5 {
+				continue
+			}
+			err := act.SendMessage(id, []byte(fmt.Sprintf("%d", i)))
 			if err != nil {
 				b.Log(err)
 			}
 		}
-		err := act2.SendMessage(id1, []byte(close))
+		err := actors[0].SendMessage(id, []byte(close))
 		if err != nil {
 			b.Log(err)
 		}
@@ -103,7 +105,7 @@ func TestSendMessage(b *testing.T) {
 	}()
 	for {
 
-		msg, ok := <-act1.recvCh
+		msg, ok := <-actors[5].recvCh
 
 		if !ok {
 			break
@@ -116,4 +118,5 @@ func TestSendMessage(b *testing.T) {
 
 	}
 
+	b.Log("Total time to receive messages:", time.Since(t1).Seconds())
 }
